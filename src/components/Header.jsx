915628e06f65494e354b40f7205d1adf0fd49999
rafgrig <grigoryanrafael515@gmail.com>
2025-02-01 18:22:58 +0400

    Write header component that has 4 links Home, Contact Us, About Us, Profile. Also write routing system

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link, useLocation,  } from "react-router";
+import profilePic from "../assets/profilePic.png"
+import "./Header.css"
+
+function Header() {
+    const {pathname} = useLocation()
+    return (
+        <header style={{backgroundColor:"FFC546"}}>
+            <div className="wrapper">
+                <Link style={{backgroundColor: pathname == "/" ? "black" : null, color: pathname == "/" ? "#FFC546" : "black",}} to="/">Home</Link>
+                <Link style={{backgroundColor: pathname == "/contact" ? "black" : null, color: pathname == "/contact" ? "#FFC546" : "black",}} to="/contact">Contact Us</Link>
+                <Link  style={{backgroundColor: pathname == "/about" ? "black" : null, color: pathname == "/about" ? "#FFC546" : "black",}} to="/about">About Us</Link>
+                <Link to="/profile"><img src={profilePic}></img></Link>
+            </div>
+                
+        </header>
+    )
+}
+
+export default Header
