7b7280ede585fa5ba7ed2f2c88d6439f1983d2e2
Sogomonyan Hayk <sogomonyanhayk7@gmail.com>
2025-02-11 19:50:30 +0400

    added some packages, rating system partially, created PostPage and added routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import Header from './components/Header';
 import Profile from './components/Profile';
 import CreatePost from './components/CreatePost';
+import PostPage from './components/PostPage';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path='/' element={<div>Home</div>}/>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/create' element={<CreatePost/>}/>
-
+        <Route path="/post/:userId/:postIndex" element={<PostPage />} />
       </Routes>
     </Router>
     
