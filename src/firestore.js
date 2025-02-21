f9c2a5f9612d54590a83bc908f81262b83db205e
NverMuxayelyan <nvermuxayelyan2009@gmail.com>
2025-02-21 18:16:08 +0400

    Add files via upload

--- src/firestore.js
+++ src/firestore.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getFirestore, doc, getDoc} from "firebase/firestore"
-import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries// Your web app's Firebase configuration
-
 const firebaseConfig = {
  apiKey: "AIzaSyCyZu0wtr8YveRdZmY2dfowH0ndzx0K92c",
  authDomain: "picture-place.firebaseapp.com",
@@ -12,8 +8,7 @@ const firebaseConfig = {
  storageBucket: "picture-place.firebasestorage.app",
  messagingSenderId: "623730100440",
  appId: "1:623730100440:web:954c2cf5b469293fbb2fad"
-};// Initialize Firebase
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
-export const auth = getAuth()
